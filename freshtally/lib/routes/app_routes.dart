a9615bd698d77d580b90452291b706dab3631e3f
Mayanja Jessy Elijah <mjessyelijah@gmail.com>
2025-07-08 15:08:07 +0300

    added the app_routes

---
+++ freshtally/lib/routes/app_routes.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+// Auth pages
+import '../pages/auth/login_page.dart';
+import '../pages/auth/create_supermarket_page.dart';
+import '../pages/auth/setup_success_page.dart';
+import '../pages/auth/join_supermarket_page.dart';
+import '../pages/auth/role_selection_page.dart';
+import '../pages/auth/staff_signup_page.dart';
+import '../pages/auth/customer_signup_page.dart';
+
+// Staff pages
+import '../pages/staff/home/home_screen.dart';
+import '../pages/staff/products/product_entry_page.dart';
+import '../pages/staff/products/edit_product_page.dart';
+import '../pages/staff/products/product_list_page.dart';
+import '../pages/staff/batches/batch_entry_page.dart';
+import '../pages/staff/shelves/shelf_mapping_page.dart';
+import '../pages/staff/shelves/smart_suggestions_page.dart';
+import '../pages/staff/expiry/expiry_tracking_page.dart';
+import '../pages/staff/expiry/expired_details_page.dart';
+import '../pages/staff/notifications/notification_center_page.dart';
+import '../pages/staff/sync/sync_status_page.dart';
+import '../pages/staff/sales/sales_page.dart';
+import '../pages/staff/analytics/dashboard_page.dart';
+import '../pages/staff/analytics/inventory_breakdown_page.dart';
+import '../pages/staff/analytics/restocking_trends_page.dart';
+import '../pages/staff/settings/user_role_page.dart';
+import '../pages/staff/settings/settings_page.dart';
