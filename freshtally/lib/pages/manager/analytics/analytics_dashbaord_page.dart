b3ebbfdbe5e770fbdc14825dfead3e3f28574388
Kala Samuel <kalasamuel79@gmail.com>
2025-07-20 19:15:43 +0300

    analytics

--- freshtally/lib/pages/manager/analytics/analytics_dashbaord_page.dart
+++ freshtally/lib/pages/manager/analytics/analytics_dashbaord_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fl_chart/fl_chart.dart';
-import 'package:intl/intl.dart';
 
 class AnalyticsDashboardPage extends StatefulWidget {
   const AnalyticsDashboardPage({super.key});
